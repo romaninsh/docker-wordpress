aec8f7efec2175ef0467159f2c087efe9ce0ac74
Romans Malinovskis <romans@agiletoolkit.org>
2015-01-27 12:48:26 +0000

    fix

---
+++ wp-config.php
@@ -0,0 +1,10 @@
+$dsn=$_ENV['DATABASE_URL'];
+preg_match( '|([a-z0-9]+)://([^:]*)(:(.*))?@([A-Za-z0-9\.-]*)(:([0-9]*))?(/([0-9a-zA-Z_/\.-]*))|', $dsn, $matches);
+
+define('DB_HOST', $matches[5]);
+define('DB_USER', $matches[2]);
+define('DB_PASSWORD', $matches[4]);
+define('DB_NAME',$matches[9]);
+
+#define('WP_HOME','http://usblog.triumph.com');
+#define('WP_SITEURL','http://usblog.triumph.com');
