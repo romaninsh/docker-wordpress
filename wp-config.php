3b16f1fa62b9221a49220950236a0da02306d6fc
Romans Malinovskis <romans@agiletoolkit.org>
2015-01-27 12:55:50 +0000

    fix

--- wp-config.php
+++ wp-config.php
@@ -8,5 +8,18 @@ define('DB_USER', $matches[2]);
 define('DB_PASSWORD', $matches[4]);
 define('DB_NAME',$matches[9]);
 
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', ''     );
+
 #define('WP_HOME','http://usblog.triumph.com');
 #define('WP_SITEURL','http://usblog.triumph.com');
+
+
+$table_prefix  = 'wp_';
+
+define('WP_DEBUG', false);
+
+if ( !defined('ABSPATH') )
+  define('ABSPATH', dirname(__FILE__) . '/');
+
+require_once(ABSPATH . 'wp-settings.php');
